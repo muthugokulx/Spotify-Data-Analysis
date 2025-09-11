510d00975414c10d418aeb7196a7201c2b0d90f8
muthugokulx <muthugokul.sr@gmail.com>
2025-09-11 22:38:34 +0530

    visualize data using mathplotlib

--- spotify-data-analysis/spotify.py
+++ spotify-data-analysis/spotify.py
@@ -5,21 +5,20 @@ import matplotlib.pyplot as plt
 import re
 
 #Set up client Credentials
-sp = spotipy.spotify(auth_manager = SpotifyClientCredentials(
+sp = spotipy.Spotify(auth_manager = SpotifyClientCredentials(
     client_id = "f630ad3f2a8f4ed2a2ac610b5121f50b",
-    client_secret = "*****************************"
+    client_secret = "aedbdbe0ec224b26b440c906344503b4"
 ))
 
 
 #Full Track URL
 
-track_url = "https://open.spotify.com/track/7qiZfU4dY1lWllzX7mPBI"
+track_url = "3h4T9Bg8OVSUYa6danHeH5"
 
 #Extract Track Id Directly from URL using Regex
 
-track_id = re.search(r'track/([a-zA-Z0-9]+)', track_url).group(1)
 
-track = sp.track(track_id)
+track = sp.track(track_url)
 print(track)
 
 #Extract Metadata
@@ -34,12 +33,25 @@ track_data = {
 }
 
 #Convert Metadata to Dataframe
-df = pd.DataFrame(track_data)
+df = pd.DataFrame([track_data])
 print("\n Print data as DataFrame")
 print(df)
 
 #Save metadata to csv
 df.to_csv("Spotify_track_data.csv", index=False)
+
+#Visualize Track Data
+
+features = ["Popularity", "Durations(minutes)"]
+values = [track_data["Popularity"], track_data["Duration (minutes)"]]
+
+plt.figure(figsize=(8,5))
+plt.bar(features, values, color = "skyblue", edgecolor = 'black')
+plt.title(f"Track Metadata for '{track_data['Track Name']}'")
+plt.ylabel('value')
+plt.show()
+
+
 
 
 
